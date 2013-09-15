89dbe284cda86130d2c65879dedf860476c2b1f0
Michael Schuenck <michaelss@gmail.com>
2013-09-15 13:01:25 -0300

    [1] Tratando o caso da URL destino ser inalcançável.

--- source/dudol/grails-app/controllers/dudol/PSController.groovy
+++ source/dudol/grails-app/controllers/dudol/PSController.groovy
@@ -9,7 +9,16 @@ import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.ObjectOutputStream
 
-import org.apache.http.client.fluent.Request
+import java.io.IOException
+
+import org.apache.http.HttpEntity
+import org.apache.http.HttpResponse
+import org.apache.http.client.ClientProtocolException
+import org.apache.http.client.ResponseHandler
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.CloseableHttpClient
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.util.EntityUtils
 
 import grails.converters.JSON
 
@@ -35,7 +44,34 @@ class PSController {
     private repassar() {
         String url = getConfMap(params.exchange, params.destino)['url'] + '?' + request.queryString
 
-        render Request.Get(url).execute().returnContent().asString()
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet(url)
+
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode()
+
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity()
+                        return entity != null ? EntityUtils.toString(entity) : null
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status)
+                    }
+                }
+
+            }
+            String responseBody = httpclient.execute(httpget, responseHandler)
+            render responseBody
+        }
+        catch (Exception e) {
+            render 'ERRO'
+        }
+        finally {
+            httpclient.close()
+        }
     }
 
     private enfileirar() {

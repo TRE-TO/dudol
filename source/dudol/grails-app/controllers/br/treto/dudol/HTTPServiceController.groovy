5650349b5441c8c77b5860429704f94330488be8
michaelss@gmail.com <michaelss@gmail.com>
2013-11-26 16:37:15 -0300

    #11 Implementada a comunicação via HTTP POST. Foram utilizadas closures na Controller HTTPService para maior reuso, já que os controles são iguais para chamadas GET e POST.

--- source/dudol/grails-app/controllers/br/treto/dudol/HTTPServiceController.groovy
+++ source/dudol/grails-app/controllers/br/treto/dudol/HTTPServiceController.groovy
@@ -6,28 +6,36 @@ import grails.transaction.Transactional
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.ObjectOutputStream
-
 import java.io.IOException
+
+import java.util.Map.Entry
 
 import org.apache.http.HttpEntity
 import org.apache.http.HttpResponse
 import org.apache.http.client.ClientProtocolException
 import org.apache.http.client.ResponseHandler
+import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.client.methods.HttpPost
 import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
 
+import org.apache.http.NameValuePair
+import org.apache.http.message.BasicNameValuePair
+
+
 @Transactional(readOnly = true)
 class HTTPServiceController {
 
-	static allowedMethods = [get: 'GET', post: 'POST']
+	static allowedMethods = [get: 'GET', post: 'POST', testPost: 'POST']
 
-	def get() {
-		if (!params.key) {
-			render(status: 400, text: 'O parâmetro "key" deve ser informado.')
-			return
-		}
+    private callAction(request, params, action) {
+        if (!params.key) {
+            render(status: 400, text: 'O parâmetro "key" deve ser informado.')
+            return
+        }
 
         HTTPService conf = HTTPService.findWhere(key: params.key)
         if (!conf) {
@@ -35,13 +43,30 @@ class HTTPServiceController {
             return
         }
 
-        // Ex: part1/part2/part3
-        def suffix = (params.opt) ? new StringBuffer('/' + params.opt) : new StringBuffer()
-        if (request.queryString) suffix << '?' + request.queryString
-        String url = conf.baseUrl + suffix.toString()
-
+        Boolean sucesso = false
+        String url = createUrl(request, params, conf)
         CloseableHttpClient httpclient = HttpClients.createDefault()
         try {
+            action(httpclient, conf, url)
+            sucesso = true
+        }
+        catch (ClientProtocolException e) {
+            int statusorig = Integer.parseInt(e.message)
+            response.status = (statusorig < 300) ? 400 : statusorig
+            registrarErro(url, response.status)
+        }
+        catch (Exception e) {
+            response.status = 500
+            registrarErro(url, response.status)
+        }
+        finally {
+            httpclient.close()
+        }
+        sucesso
+    }
+
+	def get() {
+        def action = { httpclient, conf, url ->
             HttpGet httpget = new HttpGet(url)
             ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
                 public String handleResponse(
@@ -59,27 +84,49 @@ class HTTPServiceController {
             }
             render httpclient.execute(httpget, responseHandler)
         }
-        catch (ClientProtocolException e) {
-            int statusorig = Integer.parseInt(e.message)
-            response.status = (statusorig < 300) ? 400 : statusorig
-            registrarErro(url, response.status)
-        }
-        catch (Exception e) {
-            response.status = 500
-            registrarErro(url, response.status)
-        }
-        finally {
-            httpclient.close()
-        }
+        
+        callAction(request, params, action)
 	}
 
 	def post() {
-		//TODO
+        params.remove('action')
+        params.remove('controller')
+
+		def action = { httpclient, conf, url ->
+            HttpPost httpPost = new HttpPost(url)
+
+            List <NameValuePair> nvps = new ArrayList <NameValuePair>()
+            for (Entry<String, Object> entry : params) {
+                nvps.add(new BasicNameValuePair(entry.getKey(), entry.getValue()))
+            }
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps))
+            CloseableHttpResponse response = httpclient.execute(httpPost)
+
+            try {
+                System.out.println(response.getStatusLine())
+                HttpEntity entity = response.getEntity()
+                // se fosse precisar tratar o conteudo retornado, seria aqui
+                EntityUtils.consume(entity)
+            } finally {
+                response.close()
+            }
+        }
+
+        callAction(request, params, action)
+
+        //TODO: Controle de 'retentativas' para quando não conseguir da primeira vez.
 	}
 
     private registrarErro(String url, Integer status) {
         render(status: status, text: 'Requisição inválida - Status: ' + status + '<br/>URL usada: ' + url)
         log.warn 'Requisição inválida - Status: ' + status + '<br/>URL usada: ' + url
+    }
+
+    private createUrl(request, params, conf) {
+        // Ex: part1/part2/part3
+        def suffix = (params.opt) ? new StringBuffer('/' + params.opt) : new StringBuffer()
+        if (request.queryString) suffix << '?' + request.queryString
+        conf.baseUrl + suffix.toString()
     }
 
 }
--- source/dudol/grails-app/views/layouts/main.gsp
+++ source/dudol/grails-app/views/layouts/main.gsp
@@ -48,8 +48,8 @@
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li <g:if test="${controllerName == 'sistema'}">class="active"</g:if>><a href="<g:createLink controller="sistemas"/>">Sistemas</a></li>
-            <li <g:if test="${controllerName == 'emailAdmin'}">class="active"</g:if>><a href="<g:createLink controller="email"/>">Emails</a></li>
+            <li <g:if test="${controllerName == 'HTTPServiceAdmin'}">class="active"</g:if>><a href="<g:createLink controller="HTTPServiceAdmin"/>">Serviços HTTP</a></li>
+            <li <g:if test="${controllerName == 'emailAdmin'}">class="active"</g:if>><a href="<g:createLink controller="EmailAdmin"/>">Emails</a></li>
             <li <g:if test="${controllerName == 'agendamento'}">class="active"</g:if>><a href="<g:createLink controller="agendamento"/>">Agendamento</a></li>
             <!--li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
